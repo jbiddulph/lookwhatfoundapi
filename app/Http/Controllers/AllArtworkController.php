6d1cbb68b05867cee0f50e604c2afa3353dec8ce
John Biddulph <johnbiddulph@BPS-John-MacBook-Pro.local>
2021-04-19 20:33:15 +0100

    fixed destroy

--- app/Http/Controllers/AllArtworkController.php
+++ app/Http/Controllers/AllArtworkController.php
@@ -134,7 +134,7 @@ class AllArtworkController extends Controller
      * @param  \App\Models\artwork  $artwork
      * @return \Illuminate\Http\Response
      */
-    public function destroy(artwork $id)
+    public function destroy($id)
     {
         $artwork = Artwork::find($id);
 
